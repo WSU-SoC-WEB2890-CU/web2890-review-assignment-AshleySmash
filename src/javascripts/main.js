f4ca82587f54ad4a047f4aca165fe342f2780586
Ashley Cox <ashleycox1@mail.weber.edu>
2024-09-08 22:11:40 -0600

    Finished Review

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,21 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
-import 'bootstrap'
+import "bootstrap";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const filterButtons = document.querySelectorAll(".filter-button");
+  const portfolioItems = document.querySelectorAll(".filter-by");
+
+  filterButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const filter = button.getAttribute("data-filter");
+
+      portfolioItems.forEach((item) => {
+        if (filter === "all" || item.classList.contains(filter)) {
+          item.style.display = "block";
+        } else {
+          item.style.display = "none";
+        }
+      });
+    });
+  });
+});
